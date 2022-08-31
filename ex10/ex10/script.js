3ace8baee7e6ca880ace3ca0fc6d4a5e9f3dd785
NataliRocha <110261630+NataliRocha@users.noreply.github.com>
2022-08-31 20:47:07 -0300

    up

---
+++ ex10/ex10/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+
+    var msg = window.document.getElementById ('msg')
+    var img = window.document.getElementById ('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+var hora = 18
+    msg.innerHTML = `Agora são ${hora} horas!  ` 
+    if (hora >=0 && hora <12) {
+        img.src= "1.png"
+        document.body.style.background = '669999'
+    } else if ( hora >=12 && hora <18){
+        img.src= "2.png"
+        document.body.style.background = ''
+    } else {
+    
+    img.src= "3.png"
+    document.body.style.background = 'blue'}
+    }
