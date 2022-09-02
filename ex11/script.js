496da0607a5f63fd1e471051d36fd6efe27c4a66
NataliRocha <110261630+NataliRocha@users.noreply.github.com>
2022-09-02 08:34:09 -0300

    up

---
+++ ex11/script.js
@@ -0,0 +1,24 @@
+function verificar (){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById("txtano")
+   var res = document.querySelector("div#res")
+
+if( fano.value.length === 0 || fano.value > ano){
+    alert ("[ERRO} Verifique o ano e tente novamente!")}
+else{
+var fsex= document.getElementsByName ('radsex')
+var idade = ano - Number(fano.value)
+res.innerHTML = `Idade calculada: ${idade} `
+var genero = '' 
+if (fsex[0].checked){
+    genero='homem'}
+    else if (fsex[1].checked){
+        genero = 'mulher'
+    }
+    res.style.textAlign= 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+}
+
+}
+
