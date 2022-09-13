dc8f2c828f35a209ce8eaaf5a1f2061b7283428f
Natali Kriginski Nunes da Rocha <110261630+NataliRocha@users.noreply.github.com>
2022-09-13 11:02:03 -0300

    exercícios

--- ex12/script.js
+++ ex12/script.js
@@ -1,4 +1,6 @@
 function contar(){
+   
+
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById ('txtp')
@@ -6,19 +8,34 @@ function contar(){
 
 
     if(ini.value.length ===0 ||fim.value.length ===0 || passo.value.length===0) {
-        alert('digite um número')
+        alert('Faltam dados')
     }
+   
     
     else {
-       res.innerHTML = 'Contando: '
+       res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
+        if( i<f){
+            alert('Passo Inválido! Considerando passo 1')
+            p=1
+        }
+//contagem crescente
+        if(i<f){
         for ( let c = i; c <=f; c+=p){
             res.innerHTML += `${c} \u{1F449} `
+        }   
+       
+    } else {
+        for (var c= i; c>=f; c-=p){
+//contagem regressiva
+            res.innerHTML+= `${c} \u{1F449}`
         }
-
-
     }
-}
+
+    res.innerHTML += `\u{1F3C1}`
+
+    } }
+
+    
